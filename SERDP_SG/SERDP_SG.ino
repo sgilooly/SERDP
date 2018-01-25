9e58259edba9ed0752df0d5f0289d80e4af17557
sgilooly <sem748@wildcats.unh.edu>
2018-01-25 10:02:13 -0500

    Add files via upload

---
+++ SERDP_SG/SERDP_SG.ino
@@ -0,0 +1,84 @@
+#include "Wire.h"
+#include <MS5837.h>
+//extern "C" {
+//#include "utility/twi.h" // from Wire library, so we can do bus scanning
+//}
+#define TCAADDR 0x70
+#define TCAADDR1 0x71
+#define HWSERIAL Serial1
+MS5837 sensor;
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+  Wire.begin();
+  Wire.beginTransmission(TCAADDR1);
+  Wire.write(0);
+  //Wire.write(1 << 7);
+  Wire.endTransmission();
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(0);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+void tcaselect1(uint8_t i) {
+  if (i > 7) return;
+  Wire.begin();
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(0);
+  //Wire.write(1 << 7);
+  Wire.endTransmission();
+  Wire.beginTransmission(TCAADDR1);
+  Wire.write(0);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  Serial.println("Setup Complete");
+  Serial.println("  ");
+  sensor.setModel(MS5837::MS5837_30BA);
+  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
+
+}
+
+void loop(){
+  Serial.println("Multiplexer 0:");
+  for (uint8_t index=0; index<8; index++) {
+    tcaselect(index);
+    readAndWrite();
+    delay(10);
+  }
+  Serial.println(" "); 
+  
+  Serial.println("Multiplexer 1:");
+  for (uint8_t index=0; index<8; index++) {
+    tcaselect1(index);
+    readAndWrite();
+    delay(10);
+  }
+  Serial.println(" "); 
+  
+}
+
+void readAndWrite () {
+if (!sensor.init()) {
+  delay(10);
+  if(!sensor.init()){
+    Serial1.print("0.00");
+    Serial.print(" miss ");
+  }
+}
+else {
+    sensor.read();
+    Serial.print(sensor.pressure());
+    Serial.print(" ");
+  }
+}  
+
+
---
+++ press/press.ino
@@ -0,0 +1,97 @@
+
+/* SD Wiring: 
+5V  --> 5V   GND --> GND  CLK --> 13   
+DO  --> 12   DI  --> 11   CS  --> 10
+FlexiForce: A1
+Digikey: A4 and A5 (SDA and SCL)                                                    )
+
+
+*/
+#include <MS5837.h>
+//#include <SPI.h>
+//#include <SD.h>
+#include <Wire.h>
+
+
+MS5837 sensor; // call pressure sensor "sensor"
+//int chipSelect=10; // pin used of MicroSD Card
+int times=100; //ms, time delay
+//char filename[] = "D00.CSV"; //begin with this name if no files exist
+//int flexiPin=0; //analog pin connected to FlexiForce
+//File logfile;   //create file handle   
+
+void setup() {
+Serial.begin(9600);
+/*Serial.print("Initializing SD card...");
+pinMode(chipSelect,OUTPUT);
+  // initialize the SD card
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return;
+  }
+  Serial.println("card initialized.");
+  // create a new file
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[1] = i/10 + '0';
+    filename[2] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+   if (! logfile) {
+    Serial.println("Error: couldnt create file");
+  }
+  Serial.print("Logging to: ");
+  Serial.println(filename);
+*/
+  // Initialize pressure sensor
+  Wire.begin();
+  while (!sensor.init()) {
+    Serial.println("Init failed!");
+    Serial.println("Are SDA/SCL connected correctly?");
+    Serial.println("Blue Robotics Bar30: White=SDA, Green=SCL");
+    Serial.println("\n\n\n");
+    delay(5000);
+  }
+  sensor.setModel(MS5837::MS5837_30BA);
+  sensor.setFluidDensity(997); // kg/m^3 (freshwater, 1029 for seawater)
+  
+  //logfile=SD.open(filename, FILE_WRITE); 
+  //logfile.println("Flexi, Pressure (mbar), Temp (deg C), Depth (m), Altitude (m above sea level)");
+  //logfile.close();
+  Serial.println("Pressure (mbar), Temp (deg C), Depth (m), Altitude (m above sea level)");
+}
+
+void loop() {
+  //logfile = SD.open(filename, FILE_WRITE); 
+  sensor.read(); // Update pressure and temperature readings
+  
+//if (logfile) {
+  //Serial.print(analogRead(flexiPin));
+  //Serial.print(", ");
+  Serial.print(sensor.pressure()); 
+  Serial.print(", ");
+  Serial.print(sensor.temperature()); 
+  Serial.print(", ");
+  Serial.print(sensor.depth()); 
+  Serial.print(", ");
+  Serial.println(sensor.altitude()); 
+  
+  /*logfile.print(analogRead(flexiPin));
+  logfile.print(", ");
+  logfile.print(sensor.pressure()); 
+  logfile.print(", ");
+  logfile.print(sensor.temperature()); 
+  logfile.print(", ");
+  logfile.print(sensor.depth()); 
+  logfile.print(", ");
+  logfile.println(sensor.altitude()); 
+  logfile.close();
+  */
+//}
+delay(times);
+}
+
+   
